a372f985fda021d299e616a8496bf8d7211812a1
unknown <profegranollers@gmail.com>
2018-10-19 20:31:05 +0200

    commit inicial

---
+++ src/main/java/com/pgrsoft/cursospringbatch/CursospringbatchApplication.java
@@ -0,0 +1,12 @@
+package com.pgrsoft.cursospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbatchApplication.class, args);
+	}
+}
